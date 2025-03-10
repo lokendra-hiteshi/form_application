0d885ea6078654ea58faf423cb692d7fbb641fa7
shubhamHiteshi <shubham.prajapat@hiteshi.com>
2025-03-10 19:14:57 +0530

    update bg color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -433,7 +433,7 @@ const InterviewApplicationForm: React.FC = () => {
               <div className="flex justify-between">
                 <button
                   onClick={prevStep}
-                  className="mr-2 py-2 px-4 text-white bg-gray-200 rounded"
+                  className="mr-2 py-2 px-4  bg-gray-200 rounded"
                 >
                   Back
                 </button>
